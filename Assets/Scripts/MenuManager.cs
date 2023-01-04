214f1e9bcc846c9def7f1b6a25b49d8656f67bce
JoSilvaInfo <joanafsilva.info@gmail.com>
2023-01-04 18:14:41 +0000

    Main menu; End Menu; Pause menu done. Probelm of Reset in Pause M.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseMenu;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                Continue();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    //Loads the scene in position num 1 on Build Settings
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    //Loads the scene in position num 0 on Build Settings
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Quits the game
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has QUIT the game.");
+    }
+
+    //Pause the game
+    public void Pause()
+    {
+        //opens Pause panel
+        PauseMenu.SetActive(true);
+        //Freezes time
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void Continue()
+    {
+        //closes Pause panel
+        PauseMenu.SetActive(false);
+        //Un-freezes time
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    //Reloads current scene
+    public void RestartScene()
+    {
+        //Reloads Scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  //has some problems? Idk why yu can't do anything when you reset =_=
+        Debug.Log("Scene loaded."); 
+    }
+}
